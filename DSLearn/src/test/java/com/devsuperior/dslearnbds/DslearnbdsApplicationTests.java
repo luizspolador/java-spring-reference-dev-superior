b9c3c1f9c43faa23ca6e7e90df0b56145e634f52
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-01-25 15:46:34 -0300

    DSLearn

--- DSLearn/src/test/java/com/devsuperior/dslearnbds/DslearnbdsApplicationTests.java
+++ DSLearn/src/test/java/com/devsuperior/dslearnbds/DslearnbdsApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.spolador.DSLearn;
+package com.devsuperior.dslearnbds;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsLearnApplicationTests {
+class DslearnbdsApplicationTests {
 
 	@Test
 	void contextLoads() {
