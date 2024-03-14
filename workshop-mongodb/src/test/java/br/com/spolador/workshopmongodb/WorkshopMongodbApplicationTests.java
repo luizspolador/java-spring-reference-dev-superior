06cf881d1f949305468a9b0288839c20863bd849
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-03-14 17:09:53 -0300

    projeto inicial workshop mongodb

---
+++ workshop-mongodb/src/test/java/br/com/spolador/workshopmongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spolador.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
